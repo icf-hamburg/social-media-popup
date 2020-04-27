a8edb5340d646535aa2ee7b3662a31a9fb7cc039
Johannes C <dev@johannescopeland.com>
2020-04-27 14:23:09 +0200

    Pop-Up mit Icons hinzugefügt

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
-var modal = $('<div></div>');
-modal.attr('class', 'modal-background');
-modal.attr('style', `
+/* Modal-Hintergrund */
+var modalBackground = $('<div></div>');
+modalBackground.attr('class', 'modal-background');
+modalBackground.attr('style', `
 width: 100%;
 height: 100%;
 background-color: rgba(0, 0, 0, 0.7);
@@ -11,16 +12,51 @@ justify-content: center;
 align-items: center;
 `);
 
-$(modal).appendTo('html');
+$(modalBackground).appendTo('html');
 
-var modalContent = $('<div></div>');
-modalContent.attr('class', 'modal-content');
-modalContent.attr('style', `
-width: 500px;
-height: 300px;
+/* Modal-Popop-Fenster */
+var modalWindow = $('<div></div>');
+modalWindow.attr('class', 'modal-content');
+modalWindow.attr('style', `
 background-color: white;
 border-radius: 7px;
 text-align: center;
+
+padding: 1rem;
+padding-top: 10 rem;
+
+position: relative;
 `);
 
-$(modalContent).appendTo(modal);
+$(modalWindow).appendTo(modalBackground);
+
+/* Inhalte */
+var closeButton = $('<a>+</a>');
+closeButton.attr('class', 'modal-close');
+closeButton.attr('href', '#');
+closeButton.attr('style', `
+position: absolute;
+top: 0;
+right: 0.5rem;
+
+font-size: 2rem;
+font-weight: bold;
+text-decoration: none;
+color: black;
+
+transform: rotate(45deg);
+`);
+$(closeButton).appendTo(modalWindow);
+
+function createIcon(url) {
+    var socialMediaIcon = $('<img>');
+    socialMediaIcon.attr('src', url);
+    socialMediaIcon.attr('style', 'width: 5rem;');
+
+    $(socialMediaIcon).appendTo(modalWindow);
+}
+
+createIcon('icons/facebook.svg');
+createIcon('icons/telegram.svg');
+createIcon('icons/instagram.svg');
+createIcon('icons/youtube.svg');
