1685af03d262c74ce9daf68d241b844cf8119e31
Johannes C <dev@johannescopeland.com>
2020-04-27 14:49:59 +0200

    Finished first version of pop-up window

--- popup.js
+++ popup.js
@@ -20,6 +20,7 @@ modalWindow.attr('class', 'modal-content');
 modalWindow.attr('style', `
 background-color: white;
 border-radius: 7px;
+max-width:30rem;
 text-align: center;
 
 padding: 1rem;
@@ -30,14 +31,15 @@ position: relative;
 
 $(modalWindow).appendTo(modalBackground);
 
-/* Inhalte */
+/* Button zum Schließen */
 var closeButton = $('<a>+</a>');
 closeButton.attr('class', 'modal-close');
-closeButton.attr('href', '#');
+closeButton.attr('onClick', '$(".modal-background").remove();');
 closeButton.attr('style', `
 position: absolute;
 top: 0;
 right: 0.5rem;
+cursor: pointer;
 
 font-size: 2rem;
 font-weight: bold;
@@ -48,15 +50,48 @@ transform: rotate(45deg);
 `);
 $(closeButton).appendTo(modalWindow);
 
-function createIcon(url) {
+/* Inhalte */
+function createIcon(imageUrl, linkUrl) {
+    var iconContainer = $('<a></a>');
+    iconContainer.attr('href', linkUrl);
+    iconContainer.attr('style', `
+    text-decoration: none;
+    `)
+    
     var socialMediaIcon = $('<img>');
-    socialMediaIcon.attr('src', url);
-    socialMediaIcon.attr('style', 'width: 5rem;');
+    socialMediaIcon.attr('src', imageUrl);
+    socialMediaIcon.attr('style', `
+    width: 5rem;
+    margin: 10px;
+    `);
 
-    $(socialMediaIcon).appendTo(modalWindow);
+    $(socialMediaIcon).appendTo(iconContainer);
+    $(iconContainer).appendTo(modalWindow);
 }
 
-createIcon('icons/facebook.svg');
-createIcon('icons/telegram.svg');
-createIcon('icons/instagram.svg');
-createIcon('icons/youtube.svg');
+loremIpsum = `
+    Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+`
+function createTitle() {
+    var text = $('<h3></h3>');
+    text.text('Sed ut perspiciatis');
+    $(text).appendTo(modalWindow);
+}
+
+function createText() {
+    var text = $('<p></p>');
+    text.text(loremIpsum);
+    $(text).appendTo(modalWindow);
+}
+
+
+createIcon('icons/facebook.svg', 'https://www.facebook.com/icfhamburg/');
+createIcon('icons/instagram.svg', 'https://www.instagram.com/icfhamburg/');
+createIcon('icons/telegram.svg', 'https://t.me/ICFHH');
+createIcon('icons/youtube.svg', 'https://www.youtube.com/channel/UC89iSG16TeT75MUNCQS3iag');
+
+createTitle();
+createText();
